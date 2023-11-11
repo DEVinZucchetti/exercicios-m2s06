707ffd151e2f3ba0be97f6021da72ee5b1a89033
Robert Santos <robertsantosti@outlook.com>
2023-11-10 21:30:21 -0300

    Criando rota, metodo e validações atualizar pessoa

--- app/Http/Controllers/PessoasController.php
+++ app/Http/Controllers/PessoasController.php
@@ -7,7 +7,7 @@ use App\Models\Pessoa;
 
 class PessoasController extends Controller
 {
-    public function all()
+    public function index()
     {
         try {
             $pessoas = Pessoa::all();
@@ -29,6 +29,29 @@ class PessoasController extends Controller
 
             $pessoa = Pessoa::create($request->all());
             $message = $pessoa->name." cadastrada com sucesso";
+            return $this->response($message, $pessoa);
+        } catch (\Exception $execption) {
+            return $this->response($execption->getMessage(), null, false, 500);
+        }
+    }
+
+    public function update($id, Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required | min: 3 | max: 150',
+                'cpf' => 'min: 11 | max: 20',
+                'contact' => 'max: 20',
+            ]);
+
+            $pessoa = Pessoa::find($id);
+
+            if(empty($pessoa)) {
+                return $this->response('Pessoa não encontrada', null, false, 404);
+            }
+
+            $pessoa->update($request->all());
+            $message = $pessoa->name." atualizado com sucesso.";
             return $this->response($message, $pessoa);
         } catch (\Exception $execption) {
             return $this->response($execption->getMessage(), null, false, 500);
